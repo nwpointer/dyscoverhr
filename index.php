d2d7531166da30b2035df1ebd373b4c7040b74c4
Nathan Pointer <nwpointer@gmail.com>
2014-12-19 15:02:12 -0800

    homepage content

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php include 'template/header.php' ?>
 
-<!-- <section class="row m-b-md">
+<section class="row m-b-md">
   <div class="col-sm-6">
     <h3 class="m-b-xs text-black">Dashboard</h3>
     <small>Welcome back, John Smith, <i class="fa fa-map-marker fa-lg text-primary"></i> New York City</small>
   </div>
-</section> -->
+</section>
+<hr>
+<p>
+prototype currently demos adding job listings and employees. </p>
+<p>you can also search, filter, compare and view employees profiles.</p>
 
 <?php include 'template/footer.php' ?>
